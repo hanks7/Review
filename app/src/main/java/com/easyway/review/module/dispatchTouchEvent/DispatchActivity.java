e60ecd82ba622aeeef913b3f2875ab5ea3a427b1
hjj <474664736@qq.com>
2019-11-28 09:59:41 +0800

    1.添加注释

--- app/src/main/java/com/easyway/review/module/dispatchTouchEvent/DispatchActivity.java
+++ app/src/main/java/com/easyway/review/module/dispatchTouchEvent/DispatchActivity.java
@@ -6,6 +6,13 @@ import android.view.MotionEvent;
 import com.easyway.review.R;
 import com.easyway.review.base.BaseActivity;
 
+/**
+ *
+ * @Package:        com.easyway.review.module.dispatchTouchEvent.DispatchActivity
+ * @Author:         侯建军
+ * @CreateDate:     2019/11/28 9:58
+ * @Description:    事件分发 参考文献：http://www.luyixian.cn/news_show_14683.aspx
+ */
 public class DispatchActivity extends BaseActivity {
 
     @Override
